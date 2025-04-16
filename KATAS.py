c66c7f8b3a4fa9e95112a6e714ecfe54079d33ba
MCarlos7 <cmariscal102@gmail.com>
2025-04-16 16:24:55 -0600

    Update KATAS.py

--- KATAS.py
+++ KATAS.py
@@ -1,5 +1,41 @@
 '''
 5kyu
+The Fibonacci numbers are the numbers in the following integer sequence (Fn): 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+
+such that:
+
+F(0)=0
+F(1)=1
+F(n)=F(n−1)+F(n−2)
+Given a number, say prod (for product), we search two Fibonacci numbers F(n) and F(n+1) verifying:
+
+F(n)∗F(n+1)=prod
+Your function takes an integer (prod) and returns an array/tuple (check the function signature/sample tests for the return type in your language):
+
+if F(n) * F(n+1) = prod:
+(F(n), F(n+1), true)
+If you do not find two consecutive F(n) verifying F(n) * F(n+1) = prod:
+(F(n), F(n+1), false)
+where F(n) is the smallest one such as F(n) * F(n+1) > prod.
+Examples:
+714 ---> (21, 34, true)
+--> since F(8) = 21, F(9) = 34 and 714 = 21 * 34
+
+800 --->  (34, 55, false)
+--> since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55
+
+'''
+
+def product_fib(_prod):
+    a, b = 0, 1
+    while a * b < _prod:
+        a,b = b, a+b
+    return [a, b, a*b == _prod]
+
+
+
+'''
+5kyu
 Pete likes to bake some cakes. He has some recipes and ingredients. Unfortunately he is not good in maths. Can you help him to find out, how many cakes he could bake considering his recipes?
 
 Write a function cakes(), which takes the recipe (object) and the available ingredients (also an object) and returns the maximum number of cakes Pete can bake (integer). For simplicity there are no units for the amounts (e.g. 1 lb of flour or 200 g of sugar are simply 1 or 200). Ingredients that are not present in the objects, can be considered as 0.
