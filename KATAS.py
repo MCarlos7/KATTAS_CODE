36e6cfda7a6bbe1dc819dd561c6196d4efd90dff
MCarlos7 <cmariscal102@gmail.com>
2025-05-04 23:21:50 -0600

    Update KATAS.py

--- KATAS.py
+++ KATAS.py
@@ -1,3 +1,75 @@
+'''4KYU
+Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
+
+Intervals
+Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
+
+Overlapping Intervals
+List containing overlapping intervals:
+
+[
+   [1, 4],
+   [7, 10],
+   [3, 5]
+]
+The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
+
+Examples:
+sumIntervals( [
+   [1, 2],
+   [6, 10],
+   [11, 15]
+] ) => 9
+
+sumIntervals( [
+   [1, 4],
+   [7, 10],
+   [3, 5]
+] ) => 7
+
+sumIntervals( [
+   [1, 5],
+   [10, 20],
+   [1, 6],
+   [16, 19],
+   [5, 11]
+] ) => 19
+
+sumIntervals( [
+   [0, 20],
+   [-100000000, 10],
+   [30, 40]
+] ) => 100000030
+Tests with large intervals
+Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].
+'''
+def sum_of_intervals(intervals):
+    total = 0
+    merged = []
+
+    for current in intervals:
+        a, b = sorted(current)  # Ordena el intervalo por si viene invertido
+        new_interval = [a, b]
+        updated = []
+
+        for m in merged:
+            # Si se superponen, fusionarlos
+            if not (new_interval[1] <= m[0] or new_interval[0] >= m[1]):
+                new_interval[0] = min(new_interval[0], m[0])
+                new_interval[1] = max(new_interval[1], m[1])
+            else:
+                updated.append(m)
+
+        # Agrega el nuevo intervalo fusionado a la lista
+        updated.append(new_interval)
+        merged = updated
+
+    # Sumar longitudes
+    for i in merged:
+        total += i[1] - i[0]
+
+    return total
+
 '''
 The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range must be rounded to the closest valid value.
 
