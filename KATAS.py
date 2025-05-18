c5fff2b46e671c3076380615bd2c07c98aaebd5f
MCarlos7 <cmariscal102@gmail.com>
2025-05-17 23:34:31 -0600

    Update KATAS.py

--- KATAS.py
+++ KATAS.py
@@ -1,3 +1,70 @@
+'''3kiu
+Write a function that will solve a 9x9 Sudoku puzzle. The function will take one argument consisting of the 2D puzzle array, with the value 0 representing an unknown square.
+
+The Sudokus tested against your function will be "easy" (i.e. determinable; there will be no need to assume and test possibilities on unknowns) and can be solved with a brute-force approach.
+
+For Sudoku rules, see the Wikipedia article.
+
+puzzle = [[5,3,0,0,7,0,0,0,0],
+          [6,0,0,1,9,5,0,0,0],
+          [0,9,8,0,0,0,0,6,0],
+          [8,0,0,0,6,0,0,0,3],
+          [4,0,0,8,0,3,0,0,1],
+          [7,0,0,0,2,0,0,0,6],
+          [0,6,0,0,0,0,2,8,0],
+          [0,0,0,4,1,9,0,0,5],
+          [0,0,0,0,8,0,0,7,9]]
+
+sudoku(puzzle)
+# Should return
+ [[5,3,4,6,7,8,9,1,2],
+  [6,7,2,1,9,5,3,4,8],
+  [1,9,8,3,4,2,5,6,7],
+  [8,5,9,7,6,1,4,2,3],
+  [4,2,6,8,5,3,7,9,1],
+  [7,1,3,9,2,4,8,5,6],
+  [9,6,1,5,3,7,2,8,4],
+  [2,8,7,4,1,9,6,3,5],
+  [3,4,5,2,8,6,1,7,9]]
+'''
+def sudoku(puzzle):
+    solve = True
+    for i in range(9):
+        for j in range(9):
+            if puzzle[i][j] == 0:
+                opciones = [1,2,3,4,5,6,7,8,9]
+
+                
+                for num in puzzle[i]:
+                    if num in opciones:
+                        opciones.remove(num)
+
+                
+                for fila in puzzle:
+                    if fila[j] in opciones:
+                        opciones.remove(fila[j])
+
+                
+                start_row = (i // 3) * 3
+                start_col = (j // 3) * 3
+                for r in range(3):
+                    for c in range(3):
+                        valor = puzzle[start_row + r][start_col + c]
+                        if valor in opciones:
+                            opciones.remove(valor)
+
+                if len(opciones) == 1:
+                    puzzle[i][j] = opciones[0]
+                else:
+                    puzzle[i][j] = 0
+                    solve = False
+
+    if solve:
+        return puzzle
+    else:
+        return sudoku(puzzle)
+# It's OK. I think I have a good approach, the best practices look weird xd
+
 '''
 Your task in order to complete this Kata is to write a function which formats a duration, given as a number of seconds, in a human-friendly way.
 
