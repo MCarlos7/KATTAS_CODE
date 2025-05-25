cd0112febdbef0934c4282dbb1c3efb56573eb94
MCarlos7 <cmariscal102@gmail.com>
2025-05-24 20:01:57 -0600

    Update KATTAS.js

--- KATTAS.js
+++ KATTAS.js
@@ -1,3 +1,29 @@
+/*6
+Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+Input = "The sunset sets at twelve o' clock."
+Output = "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11"
+*/
+function alphabetPosition(text) {
+  let arr = [];
+  for (let i = 0; i < text.length; i++) {
+    let lower = text[i].toLowerCase();
+    let code = lower.charCodeAt(0);
+    if (code >= 97 && code <= 122) {
+      let position = code - 96;
+      arr.push(position);
+    }
+  }
+  return arr.join(" ");
+}
+
 /* 7
 Digital root is the recursive sum of all the digits in a number.
 
