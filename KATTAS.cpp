33cc997a57484145377e74836cbe053ea267542c
MCarlos7 <cmariscal102@gmail.com>
2025-06-01 22:14:49 -0600

    Update KATTAS.cpp

--- KATTAS.cpp
+++ KATTAS.cpp
@@ -1,3 +1,31 @@
+/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+
+*/
+#include <string>
+#include <cctype>  // Para tolower
+
+bool XO(const std::string& str) {
+    int x = 0, o = 0;
+    for (char c : str) {
+        char lower = std::tolower(c);
+        if (lower == 'x') {
+            x++;
+        } else if (lower == 'o') {
+            o++;
+        }
+    }
+    return x == o;
+}
+
 /* 6
 Given an array of integers, find the one that appears an odd number of times.
 
